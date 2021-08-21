d51ac4b67749e584e86d9dafd6188d9f65e87984
Chamara madhushanka <nlc.madhushanka@gmail.com>
2021-08-21 19:47:57 +0530

    Updates

---
+++ precache-manifest.87e5c570f3eee4cb746c6ef6477cf2ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98eba46e1d8da69fec24edc0198a8a58",
+    "url": "/tmchama/index.html"
+  },
+  {
+    "revision": "92ad3443cb96b4a66311",
+    "url": "/tmchama/static/css/2.14f3cd7d.chunk.css"
+  },
+  {
+    "revision": "99e8b237b8d90d3e3b6e",
+    "url": "/tmchama/static/css/main.589d35e2.chunk.css"
+  },
+  {
+    "revision": "92ad3443cb96b4a66311",
+    "url": "/tmchama/static/js/2.359d237d.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/tmchama/static/js/2.359d237d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99e8b237b8d90d3e3b6e",
+    "url": "/tmchama/static/js/main.1bfcb1bb.chunk.js"
+  },
+  {
+    "revision": "4686e303e8b149309885",
+    "url": "/tmchama/static/js/runtime-main.d3ebbe3a.js"
+  }
+]);
